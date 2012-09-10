bb8e0432693833ae7195be17a6ac51ca31bbd94e
Jameson <j.knoll621@gmail.com>
2012-09-11 00:36:06 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ilie::Application.config.secret_token = '91bd16f572067c04672c4cbaee90d2e2d08e47c7db99208409273e2d6ff7d81731c7fc8db8e24e019f0aaafbee0b52ceb0a0c9082de36e3cc949652eeca87160'
